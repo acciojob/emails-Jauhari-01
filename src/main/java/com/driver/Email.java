46e22d0bb4a2da19c3ee625678540dbca127853b
Shubham Jauhari <90859295+Jauhari-01@users.noreply.github.com>
2023-04-28 20:28:13 +0000

    Done assignment

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,42 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(!oldPassword.equals(this.password)){
+            return;
+        }
+
+        boolean[] flags = new boolean[5];
+        //flag 0 for checking digit
+        //flag 1 for checking uppercase
+        //flag 2 for checking is character
+        //flag 3 for checking is lowerCase
+
+        for(char c : newPassword.toCharArray()){
+            if(Character.isDigit(c)){
+                flags[0] = true;
+            }
+            if(Character.isLetter(c)){
+                flags[2] = true;
+            }
+            if(Character.isUpperCase(c)){
+                flags[1] = true;
+            }
+            if(Character.isLowerCase(c)){
+                flags[3] = true;
+            }
+            if (!Character.isDigit(c)
+                && !Character.isLetter(c)
+                && !Character.isWhitespace(c) ){
+                    flags[4] = true;
+                }
+
+            for(boolean f : flags){
+                if(f==false){
+                    return;
+                }
+            }
+
+            this.password = newPassword;
+        }
     }
 }
